8f3f470137bc9f1b3c433173f4c9315cd1b25205
dmitry <pelewin@inbox.ru>
2016-07-30 19:55:54 +1200

    detail screen draft

--- src/main/webapp/app/is/is.controller.js
+++ src/main/webapp/app/is/is.controller.js
@@ -9,10 +9,20 @@
 
     var introductions = new Map();
     introductions.set('Auckland','Auckland, based around 2 large harbours, is a major city in the north of New Zealand’s North Island. In central Queen Street, the iconic Sky Tower has views of Viaduct Harbour, which is full of superyachts and lined with bars and cafes. Auckland Domain, the city’s oldest park, is based around an extinct volcano and home to the formal Wintergardens. Mission Bay Beach is minutes from Downtown.');
-    
+
     function IsController ($scope, Principal, LoginService, $state, $stateParams) {
         var vm = this;
         $scope.region = $stateParams.region;
         $scope.introductionText=introductions.get($stateParams.region);
+
+
+        $scope.labels = ['2016', '2017', '2018', '2019'];
+        $scope.series = ['Construction'];
+
+        $scope.data = [
+            [6087,6203,6213,615]
+        ];
+
+
     }
 })();
